606e59f274b41b00d719ba4ea4039fa2ce5fd066
Seniz <so9@njit.edu>
2025-02-06 19:13:10 -0500

    Added calculation class

---
+++ calculator/calculation.py
@@ -0,0 +1,12 @@
+from decimal import Decimal
+from typing import Callable
+from calculator.operations import add, subtract, multiply, divide
+
+class Calculation:
+    def __init__(self, a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]):
+        self.a = a
+        self.b = b
+        self.operation = operation
+
+    def calculate(self):
+        return self.operation(self.a, self.b)
