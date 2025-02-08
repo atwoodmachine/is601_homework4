ab9b91f035f60e3acc4a4ca871a1d811e1907c90
Seniz <so9@njit.edu>
2025-02-07 21:37:26 -0500

    Added test cases for clear and get last when history is empty

--- tests/test_history.py
+++ tests/test_history.py
@@ -26,3 +26,11 @@ def test_get_last_calc(setup_test):
     calc = Calculation(Decimal('9'), Decimal('3'), divide)
     Calculation_History.add_to_history(calc)
     assert Calculation_History.get_last_calculation() == calc, "Failed get last calculation"
+
+def test_get_last_on_empty(setup_test):
+    Calculation_History.clear_history()
+    assert(Calculation_History.get_last_calculation()) is None, "Failed get last calculation on empty history"
+
+def test_clear_history(setup_test):
+    Calculation_History.clear_history()
+    assert len(Calculation_History.get_history()) == 0, "Failed to clear history"
