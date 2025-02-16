e95f478f9818a91fd76e19f2ec28559778ba7f71
Seniz <so9@njit.edu>
2025-02-16 17:29:04 -0500

    Fixed linter errors

--- tests/test_calculation.py
+++ tests/test_calculation.py
@@ -1,13 +1,17 @@
+# pylint: disable=invalid-name
+'''Test set up for use with faker generated data'''
 from decimal import Decimal
 import pytest
 from calculator.calculation import Calculation
-from calculator.operations import add, subtract, multiply, divide
+from calculator.operations import divide
 
 def test_calculation_operation(a, b, operation, expected):
+    '''Tests calculation result'''
     calc = Calculation(a, b, operation)
     assert calc.calculate() == expected, f"Failed operation {operation.__name__} with {a} and {b}"
 
 def test_divide_by_zero():
+    '''Tests divide by zero throws error'''
     calc = Calculation(Decimal("10"), Decimal("0"), divide)
     with pytest.raises(ValueError, match="Math error: division by zero"):
         calc.calculate()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,4 @@
+'''Test main command line program'''
 import pytest
 from main import calculate_and_print
 
@@ -6,13 +7,14 @@ from main import calculate_and_print
     ("10", "2", 'subtract', "Result: 10 subtract 2 equals 8"),
     ("4", "5", 'multiply', "Result: 4 multiply 5 equals 20"),
     ("20", "4", 'divide', "Result: 20 divide 4 equals 5"),
-    ("1", "0", 'divide', "An error occurred: Math error: division by zero"),  
-    ("9", "3", 'unknown', "Unknown operation: unknown"),  
-    ("a", "3", 'add', "Invalid input: a or 3 is not a valid number"), 
+    ("1", "0", 'divide', "An error occurred: Math error: division by zero"),
+    ("9", "3", 'unknown', "Unknown operation: unknown"),
+    ("a", "3", 'add', "Invalid input: a or 3 is not a valid number"),
     ("5", "b", 'subtract', "Invalid input: 5 or b is not a valid number")
 ])
 
 def test_main(a_input, b_input, operation_input, expected_out, capsys):
+    '''Uses paramtrized data to test command line interface'''
     calculate_and_print(a_input, b_input, operation_input)
     output = capsys.readouterr()
     assert output.out.strip() == expected_out
